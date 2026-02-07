4a82a02143672382c89df2a1681487b181277503
gourisajan-CS146 <gouri.sajan@sjsu.edu>
2026-02-06 21:21:10 -0800

    Update MergeSortedArraysTest.java

--- src/test/java/MergeSortedArraysTest.java
+++ src/test/java/MergeSortedArraysTest.java
@@ -1,21 +1,29 @@
-import org.junit.*;
-public class TestMergeArrays{
-  @Test
-  public void testMerge(){
-    int[] leftArr = { 2,4,6 };
-    int[] rightArr = { 1,3,5};
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
 
-   int[]expected = {1,2,3,4,5,6}; 
-  int [] result = sortedArraysMerge.merge(leftArr,rightArr)
-    assertArrayEquals(expected,result);
-  }
+public class TestMergeArrays {
 
-public void testMergewithUnequalArray(){
-  int []leftArr = {1};
-  int [] rightArr = {2,3,5};
-  int[] expected = {1,2,3,5};
-   int [] result = sortedArraysmerge.merge(leftArr,rightArr)
-     assertArraysEquals(expected,result); 
+    @Test
+    public void testMerge() {
+        int[] leftArr = {2, 4, 6};
+        int[] rightArr = {1, 3, 5};
 
+        int[] expected = {1, 2, 3, 4, 5, 6};
+        int[] result = SortedArraysMerge.merge(leftArr, rightArr);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testMergewithUnequalArray() {
+        int[] leftArr = {1};
+        int[] rightArr = {2, 3, 5};
+
+        int[] expected = {1, 2, 3, 5};
+        int[] result = SortedArraysMerge.merge(leftArr, rightArr);
+
+        assertArrayEquals(expected, result);
+    }
 }
-    
+
+
